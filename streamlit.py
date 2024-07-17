0cab6a32d1e4abda3853591c233205c04585e316
snp8 <snoga.paris8@gmail.com>
2024-07-17 14:59:58 +0200

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from PIL import Image
-import exifread
 import io
 
 def load_image(image_file):
@@ -8,37 +7,28 @@ def load_image(image_file):
     return img
 
 def get_exif_dict(image):
-    exif_dict = {}
     try:
-        img_byte_arr = io.BytesIO()
-        image.save(img_byte_arr, format='JPEG')
-        img_byte_arr.seek(0)
-        
-        tags = exifread.process_file(img_byte_arr)
-        for tag in tags.keys():
-            if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-                exif_dict[tag] = tags[tag]
+        exif_data = image._getexif()
+        if exif_data:
+            exif_dict = {Image.TAGS.get(tag, tag): value for tag, value in exif_data.items()}
+        else:
+            exif_dict = {}
     except Exception as e:
         st.error(f"Erreur lors de l'extraction des métadonnées EXIF: {e}")
+        exif_dict = {}
     
     return exif_dict
 
 def update_exif(image, exif_dict):
     try:
-        img_byte_arr = io.BytesIO()
-        image.save(img_byte_arr, format='JPEG')
-        img_byte_arr.seek(0)
+        exif_data = {Image.TAGS.get(tag, tag): value for tag, value in exif_dict.items()}
+        image.save("edited_image.jpg", exif=exif_data)
+        st.success("Métadonnées mises à jour et image enregistrée sous 'edited_image.jpg'")
         
-        exif_bytes = exifread.makernote.remove_maker_notes(img_byte_arr.getvalue())
-        
-        for tag, value in exif_dict.items():
-            if tag in exif_bytes:
-                exif_bytes[tag] = exifread.classes.IfdTag(tag, value)
-        
-        return exif_bytes
+        return True
     except Exception as e:
         st.error(f"Erreur lors de la mise à jour des métadonnées EXIF: {e}")
-        return None
+        return False
 
 st.title("Éditeur de métadonnées EXIF")
 
@@ -65,12 +55,7 @@ if image_file:
             updated_exif_dict[tag] = new_value
 
     if st.button("Sauvegarder les modifications"):
-        updated_exif_bytes = update_exif(img, updated_exif_dict)
-        if updated_exif_bytes:
-            with open("edited_image.jpg", "wb") as edited_image_file:
-                edited_image_file.write(updated_exif_bytes)
-            st.success("Métadonnées mises à jour et image enregistrée sous 'edited_image.jpg'")
-
+        if update_exif(img, updated_exif_dict):
             # Afficher le lien pour télécharger l'image éditée
             with open("edited_image.jpg", "rb") as file:
                 btn = st.download_button(
